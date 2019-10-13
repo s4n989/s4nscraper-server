8f1447029b4abbf38c3fdf919451fcaa00c8e387
Sandeep Pandey <sandeep.pandey@practo.com>
2019-10-14 02:27:05 +0530

    Added routes for getdata and login

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const logger = require("morgan");
 const Data = require("./data");
+var cors = require('cors')
 
-const API_PORT = process.env.PORT || 3000;
+const API_PORT = process.env.PORT || 3001;
 const app = express();
 const router = express.Router();
 
@@ -34,40 +35,15 @@ router.get("/getData", (req, res) => {
   });
 });
 
-router.post("/updateData", (req, res) => {
-  const { id, update } = req.body;
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
   Data.findByIdAndUpdate(id, update, err => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true });
   });
 });
 
-router.delete("/deleteData", (req, res) => {
-  const { id } = req.body;
-  Data.findByIdAndRemove(id, err => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
-});
-
-router.post("/putData", (req, res) => {
-  let data = new Data();
-
-  const { id, message } = req.body;
-
-  if ((!id && id !== 0) || !message) {
-    return res.json({
-      success: false,
-      error: "INVALID INPUTS"
-    });
-  }
-  data.message = message;
-  data.id = id;
-  data.save(err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
+app.use(cors())
 
 app.use("/api", router);
 
